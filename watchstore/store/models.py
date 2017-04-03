51b40d9f98149a86ee826ee4ffc5dbe2d3c1c995
knpham95 <kevinpham95@hotmail.com>
2017-04-03 16:43:58 -0600

    test
    test

--- watchstore/store/models.py
+++ watchstore/store/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+	Email = models.EmailField(max_length=200, primary_key=True)
+	Password = models.CharField(min_length=200)
+	FName = models.CharField(max_length=50)
+	LName = models.CharField(max_length=50)
