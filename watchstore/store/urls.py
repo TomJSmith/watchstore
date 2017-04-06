2ba6efec86baeb0fda253e8bbeb2a254db449108
TomJSmith <thomas_smith@live.ca>
2017-04-06 00:23:28 -0600

    created a base.html where the universal navbar lives

--- watchstore/store/urls.py
+++ watchstore/store/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.storefront, name='store_front'),
+    url(r'^storefront/', views.storefront, name='store_front'),
     url(r'^product/(?P<productName>.+)/$', views.product, name='product_page'),
     url(r'^user/(?P<userName>.+)/$', views.user, name='user_profile')
 ]
--- watchstore/store/views.py
+++ watchstore/store/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-    return render(request, 'store/index.html')
+def storefront(request):
+    return render(request, 'store/storefront.html')
 
 
 def product(request, productName):
