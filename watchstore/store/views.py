42e63c0f81ff4bd02d02a2c942a1492b4c737800
TomJSmith <thomas_smith@live.ca>
2017-04-08 15:00:23 -0600

    added links to the products on the storefront that go to the products page. updated style of search page. starting work on product page.

--- watchstore/store/views.py
+++ watchstore/store/views.py
@@ -6,10 +6,10 @@ from itertools import chain
 
 
 def storefront(request):
-	featured = Product.objects.order_by('?')[:5]		#selects a random list of 5 products for featuring
-	return render(request, 'store/storefront.html',
-		{'featured': featured,}
-	)
+    watches = Product.objects.raw('SELECT * FROM store_product ORDER BY name')
+    return render(request, 'store/storefront.html',
+                  {'watches': watches, }
+                  )
 
 
 def logInSignUpChoice(request, logInSignUp):
@@ -48,30 +48,32 @@ def signup(request, userType):
 
 def product(request, productName):
     response = "You are looking at the product page for %s" % productName
-    return HttpResponse(response)
+    return render(request, 'store/productPage.html')
 
 
 def user(request, userName):
     response = "user page for user %s" % userName
     return HttpResponse(response)
-	
+
+
 def search(request):
-	return render(request, 'store/searchform.html')
-	
+    return render(request, 'store/searchform.html')
+
+
 def results(request):
-	if request.method == "POST":
-		query = request.POST["query"]
-		try:
-			p1 = Product.objects.filter(Name__icontains=query)
-			p2 = Product.objects.filter(Description__icontains=query)
-			p3 = Product.objects.filter(Brand__icontains=query)
-			p4 = Product.objects.filter(Type__icontains=query)
-			p5 = Product.objects.filter(Compatibility__icontains=query)
-			products = p1 | p2 | p3 | p4 | p5
-			return render(request, 'store/results.html',
-				{'products': products, 'query': query,}
-			)
-		except Product.DoesNotExist:
-			return HttpResponse("No search results")
-	else:
-		return redirect('store_front')
+    if request.method == "POST":
+        query = request.POST["query"]
+        try:
+            p1 = Product.objects.filter(Name__icontains=query)
+            p2 = Product.objects.filter(Description__icontains=query)
+            p3 = Product.objects.filter(Brand__icontains=query)
+            p4 = Product.objects.filter(Type__icontains=query)
+            p5 = Product.objects.filter(Compatibility__icontains=query)
+            products = p1 | p2 | p3 | p4 | p5
+            return render(request, 'store/results.html',
+                          {'products': products, 'query': query, }
+                          )
+        except Product.DoesNotExist:
+            return HttpResponse("No search results")
+    else:
+        return redirect('store_front')
